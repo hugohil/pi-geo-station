2ab3808930f2f4c148a3534b11fc3999f236075d
hugohil <hilairehugo@gmail.com>
2020-01-24 15:46:25 +0000

    update GPS module

--- gps/index.js
+++ gps/index.js
@@ -1,21 +1,27 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const nmea = require('@drivetech/node-nmea')
+const fs = require('fs')
 
 const port = '/dev/ttyAMA0'
 const baudRate = 9600
+
+const date = new Date().toLocaleString('fr-FR', {day: '2-digit', month: '2-digit', year: 'numeric'}).replace(/\//gi, '-')
+const filename = `/data/GPS-${date}.txt`
+const filestream = fs.createWriteStream(filename, {flags:'a'})
+filestream.write(`---\n${new Date().toLocaleString('fr-FR', {minute: '2-digit', hour: '2-digit', hour12: false})}\n`)
 
 console.log(`opening serial on ${port} at rate ${baudRate}`)
 
 const serial = new SerialPort(port, { baudRate })
 
-
 const parser = serial.pipe(new Readline({ delimiter: '\r\n', encoding: 'utf8' }))
 
 parser.on('data', (raw) => {
 	const data = nmea.parse(raw)
+	console.log(data)
 	if (data.valid) {
-		console.log(data)
+		filestream(JSON.stringify(data) + '\n')
 	}
 })
 
