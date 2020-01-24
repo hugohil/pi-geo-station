74fe79c352472f508dda356693448ba526f5c26d
hugohil <hilairehugo@gmail.com>
2020-01-24 17:01:13 +0000

    Add barometer logger

--- barometer/reader.py
+++ barometer/reader.py
@@ -1,9 +1,20 @@
-import time
+#!/usr/bin/env python
 
+import time 
 import hp206c
-h= hp206c.hp206c()
+from datetime import datetime 
 
+now = datetime.now()
+date = now.strftime("%d-%m-%Y")
+timelog = now.strftime("%H:%M")
+
+filename = "/data/barometer-" + date + ".txt"
+filestream = open(filename, 'a')
+filestream.write("---\n" + timelog + "\n")
+
+h= hp206c.hp206c()
 ret=h.isAvailable()
+
 if h.OK_HP20X_DEV == ret:
     print("HP20x_dev is available.")
 else:
@@ -11,8 +22,13 @@ else:
 
 while True:
     print("---")
+    
     temp=h.ReadTemperature()
     pressure=h.ReadPressure()
     altitude=h.ReadAltitude()
-    print("Temperature\t: %.2f C\nPressure\t: %.2f hPa\nAltitude\t: %.2f m" %(temp,pressure,altitude))
-    time.sleep(1)
+    
+    log = ("T %.2f C | P %.2f hPa | A %.2f m\n" %(temp,pressure,altitude))
+    print(log)
+    filestream.write(log)
+
+    time.sleep(60)
